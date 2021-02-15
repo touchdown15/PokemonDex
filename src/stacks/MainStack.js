53a4e2e6a7fb68f6c32aa063af5d8c80c0498e9b
touchdown15 <chvs1524@gmail.com>
2021-02-15 16:59:38 -0300

    Commit Inicial

---
+++ src/stacks/MainStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import SplashScreen from '../views/SplashScreen';
+import SignInScreen from '../views/SignInScreen';
+import MainScreen from '../views/MainScreen';
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <Stack.Navigator>
+    <Stack.Screen name="SplashScreen" component={SplashScreen}/>
+    <Stack.Screen name="SignInScreen" component={SignInScreen}/>
+    <Stack.Screen name="MainScreen" component={MainScreen}/>
+  </Stack.Navigator>
+);
